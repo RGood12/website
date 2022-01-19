da7ea10042a6dd9526e41dce9e4b7230cbbbe1f9
Randy Goodson <r.goodson12@gmail.com>
2022-01-18 23:07:16 -0600

    Initial version

--- site.py
+++ site.py
@@ -8,14 +8,19 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class ResumeHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("resume.html")
+
 if __name__ == "__main__":
 
     dirname = os.path.dirname(__file__)
     settings = {"template_path": os.path.join(dirname, 'templates'),
-                "static_path"  : "/var/www/randygoodson.com/"}
+                "static_path"  : os.path.join(dirname, 'static')}
 
     app =tornado.web.Application([
         (r"/", MainHandler),
+        (r"/resume", ResumeHandler),
     ], **settings)
  
     app.listen(8000)
