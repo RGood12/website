fa09cfd3cdf33b0fa35f351feb566f733f5ab064
Randy Goodson <r.goodson12@gmail.com>
2024-07-09 12:10:39 -0500

    3.0.4

--- static/darkmode.js
+++ static/darkmode.js
@@ -2,32 +2,42 @@ document.addEventListener("DOMContentLoaded", function () {
   // Check the user's dark mode preference on page load
   let isDarkModeEnabled = localStorage.getItem("darkMode") === "enabled";
 
-  // Apply dark mode if the preference is enabled
+  // Apply dark mode if the preference is enabled without transition
   if (isDarkModeEnabled) {
-    enableDarkMode();
+    enableDarkMode(false); // Pass false to skip the transition on page load
+  } else {
+    disableDarkMode(false);
   }
 
   // Button click event listener to toggle dark mode
   const darkModeToggleButton = document.getElementById("mode");
   darkModeToggleButton.addEventListener("click", toggleDarkMode);
 
+  // Function to toggle dark mode
   function toggleDarkMode() {
-    isDarkModeEnabled = !isDarkModeEnabled; // Update the variable after toggling
+    isDarkModeEnabled = !isDarkModeEnabled;
     const newMode = isDarkModeEnabled ? "enabled" : "disabled";
     setThemePreference(newMode);
     isDarkModeEnabled ? enableDarkMode() : disableDarkMode();
   }
 
-  function enableDarkMode() {
+  // Function to enable dark mode
+  function enableDarkMode(applyTransition = true) {
     document.body.classList.add("dark");
+    document.body.classList.remove("light");
     document.getElementById("mode").textContent = "☀️";
+    document.getElementById("mode").title = "Switch to Light Mode";
   }
 
-  function disableDarkMode() {
+  // Function to disable dark mode
+  function disableDarkMode(applyTransition = true) {
+    document.body.classList.add("light");
     document.body.classList.remove("dark");
     document.getElementById("mode").textContent = "🌙";
+    document.getElementById("mode").title = "Switch to Dark Mode";
   }
 
+  // Function to set theme preference in localStorage
   function setThemePreference(value) {
     localStorage.setItem("darkMode", value);
   }
@@ -44,4 +54,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
       this.classList.toggle('open');
       nav.classList.toggle('open');
     });
-
